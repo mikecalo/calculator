3badfda9291821a4cbe5acbb79530c35b82a5f7e
Michael Calandriello <michaelcole5950@gmail.com>
2022-02-24 19:23:18 -0600

    functionality updates

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const display1 = document.querySelector('.display-1');
-const display2 = document.querySelector('.display-2');
+const topDisplay = document.querySelector('.top-display');
+const mainDisplay = document.querySelector('.main-display');
 const buttons = Array.from(document.querySelectorAll('button'));
 const numbers = Array.from(document.querySelectorAll('.number'));
 const operators = Array.from(document.querySelectorAll('.operators'));
@@ -34,46 +34,76 @@ const operate = function(operator, ...arr) {
     if (operator === divide) return divide(...arr)
 }
 
+
+
+
 // Display Functions:
 const displayValue = function(e) {
     const view = e.target.value
     const span = document.createElement('span');
     span.textContent = view;
-    display2.appendChild(span);
+    mainDisplay.appendChild(span);
 }
 
+// when one of the operator functions are pressed
 const operatorFunc = function(e) {
-        const arr = Array.from(document.querySelectorAll('span')).map(x => x.innerText)
-        const operator = e.target.value
-        getDisplayValue(arr)
-        clearDisplay()
-        storeDisplayValue(arr, operator)
+    const span = document.querySelector('span');
+    const div = document.querySelector('.display-value')
+    const firstArray = Array.from(mainDisplay.querySelectorAll('span')).map(x => x.innerText)
+    const secondArray = Array.from(topDisplay.querySelectorAll('div')).map(x => x.innerText)
+    const operator = e.target.value
+    if (operator != '=') {
+        if (topDisplay.childElementCount === 0) {
+            clearDisplay()
+            storeFirstValue(firstArray, operator)
+            // console.log(firstValue, secondValue)
+        } else {
+            const firstValue = getValue(secondArray);
+            const secondValue = getValue(firstArray);
+            evaluate(firstValue, secondValue);
+        }
+    } else {
+        storeFirstValue(firstArray, operator)
+        setTimeout(clearDisplay, 1000);
+    }
+
+}
+
+function storeFirstValue(arr, operator) {
+    // logic to display value at the top of display window 
+    const value = getValue(arr)
+    const valueDiv = document.createElement('div');
+    valueDiv.className = 'display-value';
+    valueDiv.textContent = `${value}`
+    topDisplay.appendChild(valueDiv)
+    if (operator != '=') {
+        const operatorDiv = document.createElement('div');
+        operatorDiv.className = 'display-value';
+        operatorDiv.textContent = `${operator}`
+        topDisplay.appendChild(operatorDiv)
+    }
+
 }
 
 function clearDisplay() {
     const spans = Array.from(document.querySelectorAll('span'));
+    const divs = Array.from(document.querySelectorAll('.display-value'));
     spans.forEach(span => span.remove());
+    divs.forEach(div => div.remove());
 }
 
-function getDisplayValue(arr) {
-    const mappedNums = arr.map(Number)
-    const filteredNums = mappedNums.filter(Number.isInteger)
-    const joinedNums = filteredNums.join("")
-    const value = parseInt(joinedNums)
-    return value
+const getValue = function(arr) {
+    const mapped = arr.map(Number)
+    const filtered = mapped.filter(Number.isInteger)
+    return filtered.join("")
 }
 
 
 
-function storeDisplayValue(arr, operator) {
-    // logic to display value at the top of display window 
-    const value = getDisplayValue(arr)
-    const span = document.createElement('span');
-    span.className = 'display-value';
-    span.textContent = (`${value} ${operator}`)
-    display1.appendChild(span);
-
+const evaluate = function (num1, num2) {
+console.log(num1, num2);
 }
+
     
 
 
@@ -81,5 +111,5 @@ function storeDisplayValue(arr, operator) {
 numbers.forEach(number => number.addEventListener('click', displayValue))
 operators.forEach(operator => operator.addEventListener('click', operatorFunc))
 const btnClear = document.querySelector('#clear').onclick = clearDisplay
-// const evaluate = document.querySelector('.evaluate').onclick = evaluate()
+// const equal = document.querySelector('.equal').onclick = evaluate(firstValue,secondValue)
 
