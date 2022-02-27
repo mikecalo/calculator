1cf523ee384bb9783ce73e2df3b60d9226e7c38d
Michael Calandriello <michaelcole5950@gmail.com>
2022-02-26 18:35:30 -0600

    bug fixes

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const topDisplay = document.querySelector('.top-display');
 const mainDisplay = document.querySelector('.main-display');
 const buttons = Array.from(document.querySelectorAll('button'));
 const numbers = Array.from(document.querySelectorAll('.number'));
+const dot = document.querySelector('#decimal')
 const operators = Array.from(document.querySelectorAll('.operators'));
 let initialValue = mainDisplay.querySelector('.initial-value')
 
 
+
 // Math Functions:
 const add = function(firstValue, secondValue) {
-    const sum = parseInt(firstValue) + parseInt(secondValue)
+    const sum = parseFloat(firstValue) + parseFloat(secondValue)
     return sum;
 };
 
@@ -39,11 +41,15 @@ const operate = function(operator, firstValue, secondValue) {
     if (operator === 'power') return power(firstValue,secondValue)
 }
 
-
 // Display Functions:
 const displayValue = function(e) {
-    if (initialValue.classList.contains('initial-value') || topDisplay.childElementCount != 0) {
+    const solution = document.querySelectorAll('.solution')
+
+    if (e.target.value != '.') {
         initialValue.remove()
+    }
+    if (solution.length > 0) {
+        clearDisplay()
     }
     if (mainDisplay.childElementCount < 22) {
         const view = e.target.value
@@ -52,34 +58,47 @@ const displayValue = function(e) {
         span.textContent = view;
         mainDisplay.appendChild(span);     
     }
+    if (e.target.value === '.') e.target.disabled = true
 }
 
 const operatorFunc = function(e) {
     const firstArray = Array.from(mainDisplay.querySelectorAll('span')).map(x => x.innerText)
     const secondArray = Array.from(topDisplay.querySelectorAll('div')).map(x => x.innerText)
     const div = topDisplay.querySelector('.display-operator')
-    const solution = topDisplay.querySelector('.solution')
+    const solution = mainDisplay.querySelectorAll('.solution')
+
     const operator = e.target.value
-    if (operator != '=' && topDisplay.childElementCount === 0) {
+    if (operator != '=' && mainDisplay.childElementCount === 0) {
+            clearDisplay()
+            storeFirstValue(secondArray, operator)
+        } else if (operator != '=' && topDisplay.childElementCount === 0 && solution.length === 0) {
             clearDisplay()
             storeFirstValue(firstArray, operator)
-        } else if (topDisplay.childElementCount != 0) {
+        } else if (operator != '=' && topDisplay.childElementCount === 0 && solution.length > 0) {
+            clearDisplay()
+            storeFirstValue(firstArray, operator)
+        } else if (operator != '=' && topDisplay.childElementCount != 0) {
             storedOperator = div.textContent
             const getOperator = getOperatorValue(storedOperator)
-            const firstValue = getValue(secondArray);
+            const firstValue = secondArray[0]
             const secondValue = getValue(firstArray);
             storeSecondValue(firstArray);
             const solution = operate(getOperator, firstValue, secondValue);
             displaySolution(solution)
-        } else if (operator === '=' && topDisplay.childElementCount != 0) {
+        } else if (operator === '=' && topDisplay.childElementCount != 0 && mainDisplay.childElementCount != 0) {
+            storedOperator = div.textContent
             const getOperator = getOperatorValue(storedOperator)
-            const firstValue = getValue(secondArray);
+            const firstValue = secondArray[0]
             const secondValue = getValue(firstArray);
             const solution = operate(getOperator, firstValue, secondValue);
             displaySolution(solution)
+        // } else if (operator === '=' && topDisplay.childElementCount === 0) {
+        } else if (operator === '=' && topDisplay.childElementCount != 0 && mainDisplay.childElementCount === 0) {
+            const firstValue = getValue(secondArray)
+            displaySolution(firstValue)
         } else {
-            storeFirstValue(firstArray, operator)
-            setTimeout(clearDisplay, 1000);
+            const firstValue = getValue(firstArray)
+            displaySolution(firstValue)
     }
 }
 
@@ -112,15 +131,13 @@ function clearDisplay() {
     const divs = Array.from(topDisplay.querySelectorAll('div'));
     divs.forEach(div => div.remove());
     spans.forEach(span => span.remove());
-        // const initialZero = initialValue
-        // initialZero.className = 'initial-value';
-        // initialZero.textContent = '0';
-        // mainDisplay.appendChild(initialZero);
+    dot.disabled = false
 }
 
 function clearMainDisplay() {
     const spans = Array.from(document.querySelectorAll('span'));
     spans.forEach(span => span.remove());
+    dot.disabled = false
 }
 
 function clearButton() {
@@ -132,13 +149,15 @@ function clearButton() {
     initialZero.className = 'initial-value';
     initialZero.textContent = '0';
     mainDisplay.appendChild(initialZero);
-
+    dot.disabled = false
 }
 
 const getValue = function(arr) {
+    const joined = arr.join("")
     const mapped = arr.map(Number)
-    const filtered = mapped.filter(Number.isInteger)
-    return filtered.join("")
+    // const filtered = mapped.filter(Number.isInteger)
+    if (arr.includes('.') || mapped[0] === parseFloat(joined)) return (parseFloat(joined))
+    return parseInt(joined)
 }
 
 const getOperatorValue = function(storedOperator) {
@@ -149,19 +168,19 @@ const getOperatorValue = function(storedOperator) {
     if (storedOperator === '^') return 'power'
 }
 
-// STILL NEED TO WORK ON:
-// 1. top display clearing when solution is executed. Need it to show equation.
-// 2. add keyboard functionality.
-// 3. shrink displays if numbers start to overflow and eventually stop allowing users to enter numbers.  
-
 
 const displaySolution = function(solution) {
     clearDisplay()
+    roundedSolution = Math.round((solution * 10000000)) / 10000000
     const solutionDisplay = document.createElement('span');
     solutionDisplay.className = 'solution';
-    solutionDisplay.textContent = `${solution}`
-    mainDisplay.appendChild(solutionDisplay);
-
+    if (solution === Infinity) {
+        alert("Can't divide by zero!!")
+        clearButton()
+    } else {
+        solutionDisplay.textContent = `${roundedSolution}`
+        mainDisplay.appendChild(solutionDisplay);
+    }
 }
 
 // Events
@@ -169,4 +188,6 @@ numbers.forEach(number => number.addEventListener('click', displayValue))
 operators.forEach(operator => operator.addEventListener('click', operatorFunc))
 const btnClear = document.querySelector('#clear').onclick = clearButton
 const btnClearEntry = document.querySelector('#clear-entry').onclick = clearMainDisplay
+// window.addEventListener('keydown', displayValue);
+// window.addEventListener('keydown', operatorFunc);
 
